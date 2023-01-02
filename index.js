dfb367dbf90a3da54d4b57e24c7424e7ffaef2c9
mahmud035 <mahamudulhasan4148@gmail.com>
2023-01-02 09:05:11 +0600

    connected with MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('colors');
 require('dotenv').config();
 
@@ -16,6 +17,29 @@ app.get('/', (req, res) => {
 });
 
 //* Mongodb Atlas
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yeflywl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log('Database connected'.yellow.italic);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+};
+
+dbConnect();
+
+//* Collections
+const allFactCollection = client.db('factShareDBUser').collection('allFact');
 
 //* -------------------------GET(READ)-------------------------
 
